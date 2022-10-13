055914c40571ee328cdd16f91668982289a3bfc0
Tim-Poels <timpoels2000@gmail.com>
2022-10-13 15:15:43 +0200

    all packages added - created environment for engines/visduals for the board

--- src/index.js
+++ src/index.js
@@ -1,9 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Chessboard from "chessboardjsx";
+import { Chess } from 'chess.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
-  <div>ghello</div>
+  <Chessboard position="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" ></Chessboard>
 );
 
+
+
+const playWhite = (board) => {
+  let moves = board.moves()
+  let move = moves[Math.floor(Math.random() * moves.length)]
+  board.move(move)
+
+  if (!board.isGameOver()) {
+    root.render(
+      <Chessboard position={ board.fen() } ></Chessboard>
+    );
+    setTimeout(() => {
+      return playBlack(board)
+    }, 1000)
+  }
+  else return board
+}
+
+const playBlack = (board) => {
+  let moves = board.moves()
+  let move = moves[Math.floor(Math.random() * moves.length)]
+  board.move(move)
+
+  if (!board.isGameOver()) {
+    root.render(
+      <Chessboard position={ board.fen() } ></Chessboard>
+    );
+    setTimeout(() => {
+      return playWhite(board)
+    }, 1000)
+  }
+  else return board
+}
+
+const board = new Chess
+
+let result = playWhite(board)
+
+console.log(result)
